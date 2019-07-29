a499e766a12db27df15759e8e18fddaa522c39c3
James Tang <jytang10@gmail.com>
2019-07-29 05:24:14 +0000

    Drills01 work

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,6 +1,10 @@
 
-function sumArray(){
-	
+function sumArray( arr ){
+  var answer = 0;
+  for (var i = 0; i < arr.length; i++) {
+    answer += arr[i];
+  }
+  return answer;
 }
 
 function fitWithinVal(){
