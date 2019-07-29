0fcbb52ac34b6367f49d66aefb13a7afb9492ed4
James Tang <jytang10@gmail.com>
2019-07-29 05:28:43 +0000

    Completed fitWithinVal

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -7,8 +7,14 @@ function sumArray( arr ){
   return answer;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(arr, num){
+  var sumNum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (sumNum + arr[i] > sumNum) {
+      return sumNum;
+    }
+    sumNum += arr[i];
+  }
 }
 
 function getAllNamesShorterThan(){
