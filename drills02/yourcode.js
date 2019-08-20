7d36a87090f750ead47a5da2274856aeed023e0c
James Tang <jytang10@gmail.com>
2019-08-20 06:27:41 +0000

    Completed drills02 Problem 1

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,7 +1,13 @@
 
 
-function countOccurences(){
-
+function countOccurences(arr, word){
+  let count = 0;
+  for(var i = 0; i < arr.length; i++) {
+    if (arr[i] === word) {
+      count++;
+    }
+  }
+  return count;
 }
 
 function wordLengths(){
