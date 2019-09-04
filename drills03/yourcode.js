3f5ef10247505e90395329674664f2b5e5f53771
James Tang <jytang10@gmail.com>
2019-09-04 22:36:00 +0000

    Completed getPathPats

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -4,8 +4,29 @@ function getPath(str){
   return str.split('/');
 }
 
-function getPathParts(){
-
+function getPathParts(str){
+  let answerObj = {
+    protocol: null,
+    host: null,
+    port: null,
+    path: null,
+    file: null,
+  }
+  let firstSplit = str.split(':');
+  let hostSplit = firstSplit[1].split('//');
+  let thirdSplit = firstSplit[2].split('/');
+  console.log(thirdSplit);
+  answerObj.protocol = firstSplit[0];
+  answerObj.host = hostSplit[1];
+  answerObj.port = parseInt(thirdSplit[0]);
+  if (thirdSplit.length > 3) {
+    answerObj.path = thirdSplit[1] + '/' + thirdSplit[2];
+    answerObj.file = thirdSplit[3];
+  } else {
+    answerObj.path = thirdSplit[1];
+    answerObj.file = thirdSplit[2];
+  }
+  return answerObj;
 }
 
 function getCapitalCount(){
