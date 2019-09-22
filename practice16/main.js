1d2668d0f2c6af1719b85ed27817a385273c71d0
James Tang <jytang10@gmail.com>
2019-09-22 02:58:48 +0000

    Wrote pseudocode

---
+++ practice16/main.js
@@ -0,0 +1,12 @@
+
+
+function factor_of_sequence(factor) {
+  let numArr = [];
+  for ( let index = 0; i < 10; i++) {
+    let num = index * factor;
+    if (num % factor)
+  }
+  return numArr;
+}
+
+factor_of_sequence(2);
