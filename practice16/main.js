f2783f5de383fc806332a4e3f93b97c7482ad29b
James Tang <jytang10@gmail.com>
2019-09-22 03:11:27 +0000

    Completed factor_of_sequence

--- practice16/main.js
+++ practice16/main.js
@@ -2,11 +2,14 @@
 
 function factor_of_sequence(factor) {
   let numArr = [];
-  for ( let index = 0; i < 10; i++) {
-    let num = index * factor;
-    if (num % factor)
+  for ( let index = 0; index < 11; index++) {
+    if ((index+factor) % 3 === 0) {
+      numArr.push(index+factor)
+    } else {
+      numArr.push(0)
+    }
   }
   return numArr;
 }
 
-factor_of_sequence(2);
+console.log(factor_of_sequence(2));
