dafed2c7fc3447ea7aea82089cacd44dc69b7e9e
James Tang <jytang10@gmail.com>
2019-10-06 06:58:02 +0000

    Worked on PP20

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,13 @@
-answer.js
+var input_array = [5,3,2,5,1];
+
+function randomize_array(arr){
+  let shuffle_arr = [];
+    for(var i = 0; i < 5; i++){
+      shuffle_arr.push(arr.pop())
+    }
+  
+  return shuffle_arr;
+}
+
+var output = randomize_array(input_array);
+console.log(output);
