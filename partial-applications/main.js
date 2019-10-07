91c888899466bcd301a8c6e46e5c33124c1a46e5
James Tang <jytang10@gmail.com>
2019-10-07 21:52:33 +0000

    Added 3 param variation

--- partial-applications/main.js
+++ partial-applications/main.js
@@ -14,3 +14,25 @@ const sum = (x, y) => {
   }
   return z => x + z;
 }
+
+
+
+
+const sum = ( num1, num2, num3 ) => num1 + num2 + num3;
+
+const func = partial(sum , 1);
+const output = func(2,3);
+console.log(output === 6) //===> true
+
+
+const func2 = partial(sum);
+const output = func(1,2,3);
+console.log(output === 6) //===> true
+
+
+const func3 = partial(sum , 1,2);
+const output = func(3);
+console.log(output === 6) //===> true
+
+
+const partial = () => {};
